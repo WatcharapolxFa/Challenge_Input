4f32635a33101e4c5746f9eabd82dceed2ce7e76
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-14 01:24:49 +0700

    if && else

--- Challenge_Input/Source.cpp
+++ Challenge_Input/Source.cpp
@@ -1,10 +1,48 @@
 #include<stdio.h>
 #include<math.h>
-int main ()
+int num1 = 0, num2 = 0;   // In put Number 
+
+float average = 0, SD = 0, sum = 0, nSum = 0;   // SD = Standard Deviation
+
+
+float Average(float a, float b)
 {
-	int num1 = 0 , num2 = 0;
+
+    float AVG;
+    AVG = a / b;
 
 
+    return AVG;
+}
+
+/*
+float standardDeviation(float SD)
+{
+  sqrt();
 
 
+ return
+
+
+}
+*/
+
+
+int main()
+{
+    scanf("%d %d", &num1, &num2);
+    if (num1 < num2)
+    {
+        for (;num1 <= num2; num1++)
+        {
+            printf("%d ", num1);
+            sum = sum + num1;
+            nSum = nSum + 1;
+        }
+        average = Average(sum, nSum);
+        printf("%.2f ", average);
+    }
+
+
+    return 0;
 }
