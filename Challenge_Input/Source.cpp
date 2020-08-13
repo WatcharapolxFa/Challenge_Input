b2fa39b31ccd3b9e75dbdff744f1096f260a3343
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-14 01:57:26 +0700

    if && else if && else && avg

--- Challenge_Input/Source.cpp
+++ Challenge_Input/Source.cpp
@@ -2,17 +2,17 @@
 #include<math.h>
 int num1 = 0, num2 = 0;   // In put Number 
 
-float average = 0, SD = 0, sum = 0, nSum = 0;   // SD = Standard Deviation
+float average = 0, SD = 0, sum = 0, nSum = 0;   // SD = Standard Deviation 
 
 
-float Average(float a, float b)
+float Average(float xsum, float xnsum) // xsum = sum xnsum = nsum
 {
 
     float AVG;
-    AVG = a / b;
+    AVG = xsum / xnsum;
 
 
-    return AVG;
+    return AVG; //AVG put to Average()
 }
 
 /*
@@ -30,19 +30,47 @@ float standardDeviation(float SD)
 
 int main()
 {
-    scanf("%d %d", &num1, &num2);
-    if (num1 < num2)
+
+    if (scanf_s("%d %d", &num1, &num2))
     {
-        for (;num1 <= num2; num1++)
+        if (num1 < num2)
         {
-            printf("%d ", num1);
-            sum = sum + num1;
-            nSum = nSum + 1;
+            for (;num1 <= num2; num1++)
+            {
+                printf(" %d ", num1);
+                sum = sum + num1;
+                nSum = nSum + 1;
+            }
+            average = Average(sum, nSum);
+            printf("\n Average= %.1f ", average);
         }
-        average = Average(sum, nSum);
-        printf("%.2f ", average);
+        else if (num1 > num2)
+        {
+            for (;num1 >= num2; num1--)
+            {
+                printf(" %d ", num1);
+                sum = sum + num1;
+                nSum = nSum + 1;
+            }
+            average = Average(sum, nSum);
+            printf("\n Average= %.1f ", average);
+        }
+        else
+        {
+            for (;num1 == num2; num1--)
+            {
+                printf(" %d ", num1);
+                sum = sum + num1;
+                nSum = nSum + 1;
+            }
+            average = Average(sum, nSum);
+            printf("\n Average= %.1f ", average);
+        }
+
     }
-
-
+    else
+    {
+        printf(" Wrong In put integer number ");
+    }
     return 0;
 }
